fe3c9c729a0939b02645fd4593507145a39797e7
Bill Showers <william.showers@ucdenver.edu>
2024-12-25 10:55:21 -0700

    Test app code
    Test app with activity through chapter 2

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,16 @@
+// Add CSS from global.css to app
+import '@/app/ui/global.css';
+// Import fonts
+import {inter} from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// Import Inter font from next/font/google module
+import {Inter} from 'next/font/google';
+
+// Specify subset of font to load
+// https://fonts.google.com/knowledge/glossary/subsetting
+// Appears to select a subset of characters (i.e. latin
+// characters) instead of loading characters from other 
+// writing systems?
+export const inter = Inter({subsets: ['latin']});
