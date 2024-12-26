5360bceeb8f879c1254aaed19045007942ed8c2e
Bill Showers <william.showers@ucdenver.edu>
2024-12-26 13:08:03 -0700

    Chapter 3

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -10,6 +10,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* Apply font in className attribute */}
+      {/* Backticks do not create a string. They appear to be necessary for the className to be interpereted. What are the backticks? */}
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,5 +1,7 @@
 // Import Inter font from next/font/google module
 import {Inter} from 'next/font/google';
+// Lusitania secondary font
+import{Lusitana} from 'next/font/google';
 
 // Specify subset of font to load
 // https://fonts.google.com/knowledge/glossary/subsetting
@@ -7,3 +9,6 @@ import {Inter} from 'next/font/google';
 // characters) instead of loading characters from other 
 // writing systems?
 export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana(
+    {subsets: ['latin'], 
+    weight: ["400", "700"]});
